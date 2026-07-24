dde4451af563347d1ddc033880d3c7525f4b601b
pujandhakal <dhakalpujan72@gmail.com>
2026-07-24 21:14:25 +0545

    Added Student id & name

--- yatritech/lib/screens/School/Conductor/student_list.dart
+++ yatritech/lib/screens/School/Conductor/student_list.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Student {
+  final String id;
+  final String name;
+
+  Student({required this.id, required this.name});
+}
+
 class StudentList extends StatefulWidget {
   const StudentList({super.key});
 
